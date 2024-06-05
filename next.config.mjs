2202c03ccbc2490517d80d5093f505d3f6f2ceab
Misha Vynnyk <vinnikmisha94@gmail.com>
2024-06-05 18:18:10 +0200

    deploy next action

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/portfolio-next.js",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
