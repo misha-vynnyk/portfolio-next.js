25ab976063621b790d79e13bd5baa7ff60fb2d5e
Misha Vynnyk <vinnikmisha94@gmail.com>
2024-06-05 19:26:38 +0200

    second deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/portfolio-next.js",
+  // basePath: "/portfolio-next.js",
+  // assetPrefix: '/portfolio-next.js',
+  images: {
+    loader: 'imgix',
+    path: '/',
+  },
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
