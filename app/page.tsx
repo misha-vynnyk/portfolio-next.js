cdd1c0f0d9c75aadae05aeebfa207b8a8bf8d117
Misha Vynnyk <vinnikmisha94@gmail.com>
2024-06-06 18:05:56 +0200

    add testimonials section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Clients from "@/components/Clients";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -13,6 +14,7 @@ const Home = () => {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
       </div>
     </main>
   );
